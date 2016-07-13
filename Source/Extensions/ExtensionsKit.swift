0d7a3a8f87fc884fbbe3e82e1e23a71f015af1b7
Vlad Getman <ibrightsider@gmail.com>
2016-07-13 09:36:48 +0300

    add NSRangeFromRange

--- Source/Extensions/ExtensionsKit.swift
+++ Source/Extensions/ExtensionsKit.swift
@@ -225,6 +225,13 @@ extension String {
         return nil
     }
     
+    func NSRangeFromRange(range : Range<String.Index>) -> NSRange {
+        let utf16view = self.utf16
+        let from = String.UTF16View.Index(range.startIndex, within: utf16view)
+        let to = String.UTF16View.Index(range.endIndex, within: utf16view)
+        return NSMakeRange(utf16view.startIndex.distanceTo(from), from.distanceTo(to))
+    }
+    
     public func sizeForMaxSize(textSize: CGSize, font: UIFont) -> CGSize {
         var result = CGSizeMake(0, font.lineHeight)
         if (self.length > 0) {
