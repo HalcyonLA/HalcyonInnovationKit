31d48dfe4e4c077869a83fb01df052203fb4c064
Vlad Getman <ibrightsider@gmail.com>
2016-07-13 09:41:12 +0300

    make public some extensions

--- HalcyonInnovationKit.podspec
+++ HalcyonInnovationKit.podspec
@@ -6,7 +6,7 @@ s.name = "HalcyonInnovationKit"
 s.module_name = "HalcyonInnovationKit"
 s.summary = "Halcyon Innovation development kit."
 s.requires_arc = true
-s.version = "1.0.12"
+s.version = "1.0.13"
 s.author = { "Vlad Getman" => "ibrightsider@gmail.com" }
 
 s.license = { :type => "MIT", :file => "LICENSE" }
--- Source/Extensions/ExtensionsKit.swift
+++ Source/Extensions/ExtensionsKit.swift
@@ -215,7 +215,7 @@ extension String {
         return NSMakeRange(0, self.length)
     }
     
-    func rangeFromNSRange(nsRange : NSRange) -> Range<String.Index>? {
+    public func rangeFromNSRange(nsRange : NSRange) -> Range<String.Index>? {
         let from16 = utf16.startIndex.advancedBy(nsRange.location, limit: utf16.endIndex)
         let to16 = from16.advancedBy(nsRange.length, limit: utf16.endIndex)
         if let from = String.Index(from16, within: self),
@@ -225,7 +225,7 @@ extension String {
         return nil
     }
     
-    func NSRangeFromRange(range : Range<String.Index>) -> NSRange {
+    public func NSRangeFromRange(range : Range<String.Index>) -> NSRange {
         let utf16view = self.utf16
         let from = String.UTF16View.Index(range.startIndex, within: utf16view)
         let to = String.UTF16View.Index(range.endIndex, within: utf16view)
