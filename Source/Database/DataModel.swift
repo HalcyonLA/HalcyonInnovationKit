111102cab8df4f2c8e143db4f9ba40892a1ce5ef
Vlad Getman <ibrightsider@gmail.com>
2016-08-18 21:55:08 +0300

    improve data model

--- Source/Database/DataModel.swift
+++ Source/Database/DataModel.swift
@@ -16,6 +16,10 @@ public class DataModel: NSObject {
     public static let shared = DataModel()
     public static var dbName = AppName()
     
+    private var _managedObjectModel: NSManagedObjectModel?
+    private var _persistentStoreCoordinator: NSPersistentStoreCoordinator?
+    private var _managedObjectContext: NSManagedObjectContext?
+    
     let log = XCGLogger.defaultInstance()
     
     // MARK: - Core Data stack
@@ -25,60 +29,81 @@ public class DataModel: NSObject {
         return urls[urls.count-1]
     }()
     
-    private lazy var managedObjectModel: NSManagedObjectModel = {
-        // The managed object model for the application. This property is not optional. It is a fatal error for the application not to be able to find and load its model.
-        let modelURL = NSBundle.mainBundle().URLForResource(dbName, withExtension: "momd")!
-        return NSManagedObjectModel(contentsOfURL: modelURL)!
-    }()
-    
-    private lazy var persistentStoreCoordinator: NSPersistentStoreCoordinator = {
-        // The persistent store coordinator for the application. This implementation creates and returns a coordinator, having added the store for the application to it. This property is optional since there are legitimate error conditions that could cause the creation of the store to fail.
-        // Create the coordinator and store
-        let coordinator = NSPersistentStoreCoordinator(managedObjectModel: self.managedObjectModel)
-        let url = self.applicationDocumentsDirectory.URLByAppendingPathComponent("\(dbName).sqlite")
-        var failureReason = "There was an error creating or loading the application's saved data."
-        do {
-            try coordinator.addPersistentStoreWithType(NSSQLiteStoreType, configuration: nil, URL: url, options: nil)
-        } catch {
-            
-            func reportAboutBadPersistentStore() {
-                // Report any error we got.
-                var dict = [String: AnyObject]()
-                dict[NSLocalizedDescriptionKey] = "Failed to initialize the application's saved data"
-                dict[NSLocalizedFailureReasonErrorKey] = failureReason
-                
-                dict[NSUnderlyingErrorKey] = error as NSError
-                let wrappedError = NSError(domain: "YOUR_ERROR_DOMAIN", code: 9999, userInfo: dict)
-                // Replace this with code to handle the error appropriately.
-                // abort() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
-                NSLog("Unresolved error \(wrappedError), \(wrappedError.userInfo)")
-                abort()
+    private var managedObjectModel: NSManagedObjectModel {
+        get {
+            if let moc = _managedObjectModel {
+                return moc
+            } else {
+                // The managed object model for the application. This property is not optional. It is a fatal error for the application not to be able to find and load its model.
+                let modelURL = NSBundle.mainBundle().URLForResource(DataModel.dbName, withExtension: "momd")!
+                _managedObjectModel = NSManagedObjectModel(contentsOfURL: modelURL)!
+                return _managedObjectModel!
             }
-            
-            do {
-                try NSFileManager.defaultManager().removeItemAtPath(url.path!)
+        }
+    }
+    
+    public var persistentStoreCoordinator: NSPersistentStoreCoordinator {
+        get {
+            if let coordinator = _persistentStoreCoordinator {
+                return coordinator
+            } else {
+                // The persistent store coordinator for the application. This implementation creates and returns a coordinator, having added the store for the application to it. This property is optional since there are legitimate error conditions that could cause the creation of the store to fail.
+                // Create the coordinator and store
+                let coordinator = NSPersistentStoreCoordinator(managedObjectModel: self.managedObjectModel)
+                let url = self.applicationDocumentsDirectory.URLByAppendingPathComponent("\(DataModel.dbName).sqlite")
+                var failureReason = "There was an error creating or loading the application's saved data."
                 do {
                     try coordinator.addPersistentStoreWithType(NSSQLiteStoreType, configuration: nil, URL: url, options: nil)
                 } catch {
-                    reportAboutBadPersistentStore()
+                    
+                    func reportAboutBadPersistentStore() {
+                        // Report any error we got.
+                        var dict = [String: AnyObject]()
+                        dict[NSLocalizedDescriptionKey] = "Failed to initialize the application's saved data"
+                        dict[NSLocalizedFailureReasonErrorKey] = failureReason
+                        
+                        dict[NSUnderlyingErrorKey] = error as NSError
+                        let wrappedError = NSError(domain: "YOUR_ERROR_DOMAIN", code: 9999, userInfo: dict)
+                        // Replace this with code to handle the error appropriately.
+                        // abort() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
+                        NSLog("Unresolved error \(wrappedError), \(wrappedError.userInfo)")
+                        abort()
+                    }
+                    
+                    do {
+                        try NSFileManager.defaultManager().removeItemAtPath(url.path!)
+                        do {
+                            try coordinator.addPersistentStoreWithType(NSSQLiteStoreType, configuration: nil, URL: url, options: nil)
+                        } catch {
+                            reportAboutBadPersistentStore()
+                        }
+                    } catch {
+                        reportAboutBadPersistentStore()
+                    }
+                    
                 }
-            } catch {
-                reportAboutBadPersistentStore()
+                
+                _persistentStoreCoordinator = coordinator
+                return _persistentStoreCoordinator!
             }
-            
         }
-        
-        return coordinator
-    }()
+    }
     
-    public lazy var managedObjectContext: NSManagedObjectContext = {
-        // Returns the managed object context for the application (which is already bound to the persistent store coordinator for the application.) This property is optional since there are legitimate error conditions that could cause the creation of the context to fail.
-        let coordinator = self.persistentStoreCoordinator
-        var managedObjectContext = NSManagedObjectContext()
-
-        managedObjectContext.persistentStoreCoordinator = coordinator
-        return managedObjectContext
-    }()
+    public var managedObjectContext: NSManagedObjectContext {
+        get {
+            if let moc = _managedObjectContext {
+                return moc
+            } else {
+                /// Returns the managed object context for the application (which is already bound to the persistent store coordinator for the application.) This property is optional since there are legitimate error conditions that could cause the creation of the context to fail.
+                let coordinator = self.persistentStoreCoordinator
+                var managedObjectContext = NSManagedObjectContext()
+                
+                managedObjectContext.persistentStoreCoordinator = coordinator
+                _managedObjectContext = managedObjectContext
+                return _managedObjectContext!
+            }
+        }
+    }
     
     // MARK: - Core Data Saving support
     
@@ -124,18 +149,54 @@ public class DataModel: NSObject {
     public class func fetchEntity<T: NSManagedObject>(entity: T.Type, predicate: NSPredicate?, descriptors: [NSSortDescriptor]? = nil) -> [T]? {
         let moc = DataModel.shared.managedObjectContext
         
-        let fetch = NSFetchRequest.init()
-        fetch.includesPendingChanges = true
-        fetch.entity = NSEntityDescription.entityForName("\(entity)", inManagedObjectContext: moc)
-        fetch.returnsObjectsAsFaults = false
-        fetch.predicate = predicate
-        fetch.sortDescriptors = descriptors
+        let fetchRequest = NSFetchRequest.init()
+        fetchRequest.includesPendingChanges = true
+        fetchRequest.entity = NSEntityDescription.entityForName("\(entity)", inManagedObjectContext: moc)
+        fetchRequest.returnsObjectsAsFaults = false
+        fetchRequest.predicate = predicate
+        fetchRequest.sortDescriptors = descriptors
         
         do {
-            let results = try moc.executeFetchRequest(fetch)
+            let results = try moc.executeFetchRequest(fetchRequest)
             return results as? [T]
         } catch {
             return nil
+        }
+    }
+    
+    public class func fetchEntity<T: NSManagedObject>(entity: T.Type, fetchModificate: (fetchRequest: NSFetchRequest) -> Void) -> [T]? {
+        let moc = DataModel.shared.managedObjectContext
+        
+        let fetchRequest = NSFetchRequest.init()
+        fetchRequest.includesPendingChanges = true
+        fetchRequest.entity = NSEntityDescription.entityForName("\(entity)", inManagedObjectContext: moc)
+        fetchRequest.returnsObjectsAsFaults = false
+        
+        fetchModificate(fetchRequest: fetchRequest)
+        
+        do {
+            let results = try moc.executeFetchRequest(fetchRequest)
+            return results as? [T]
+        } catch {
+            return nil
+        }
+    }
+    
+    public class func resetAll() {
+        let model = DataModel.shared
+        if let store = model.persistentStoreCoordinator.persistentStores.last {
+            let storeURL = store.URL!
+            do {
+                try model.persistentStoreCoordinator.removePersistentStore(store)
+                try NSFileManager.defaultManager().removeItemAtURL(storeURL)
+                model._persistentStoreCoordinator = nil
+                model._managedObjectContext = nil
+                model._managedObjectModel = nil
+                //called for reinitiate coordinator
+                model.persistentStoreCoordinator
+            } catch {
+                
+            }
         }
     }
     
